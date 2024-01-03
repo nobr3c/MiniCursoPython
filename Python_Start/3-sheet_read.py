c84fc8bf425f162e0f2c4a525c5a327c3e568da7
Nobr3c <jnobr3c+github@gmail.com>
2024-01-02 22:54:46 -0300

    :books: Aula 02 - Parte 2 - Python

---
+++ Python_Start/3-sheet_read.py
@@ -0,0 +1,17 @@
+from openpyxl import load_workbook
+
+# 1 - Lê pasta de trabalho e planilha 
+wb = load_workbook("data/pivot_table.xlsx")
+sheet = wb["Relatorio"]
+
+# 2 - Acessando um valor especifico
+print(sheet["A3"].value)
+print(sheet["B3"].value)
+
+# 3 - Iterando valores por meio de Loop
+
+for i in range(2,6):
+    ano = sheet["A%s" %i].value
+    am =  sheet["B%s" %i].value
+    bt =  sheet["C%s" %i].value
+    print("{0} O Aston martin vendeu {1} e o Bentley vendeu {2}".format(ano, am, bt))
