cc3bee9b92a7f2b93184bdea09976b53ab42d5df
Nobr3c <jnobr3c+github@gmail.com>
2024-01-02 23:18:34 -0300

    :books: Aula 02 - Parte 3 - Python

---
+++ Python_Start/4-add_chart.py
@@ -0,0 +1,46 @@
+from openpyxl import load_workbook
+from openpyxl.chart import BarChart, Reference
+
+# 1 - Lê pasta de trabalho e planilha 
+wb = load_workbook("data/pivot_table.xlsx")
+sheet = wb["Relatorio"]
+
+# 2 - referências das linhas e Colunas
+min_column = wb.active.min_column
+max_column = wb.active.max_column
+min_row = wb.active.min_row
+max_row = wb.active.max_row
+print(min_column)
+print(max_column)
+print(min_row)
+print(max_row)
+
+# 3 - Adicionando Dados e Categorias no Gráfico
+barchart = BarChart()
+
+data = Reference(
+    sheet,
+    min_col = min_column + 1, #para nao pegar o ano coloca min_column + 1 
+    max_col = max_column,
+    min_row =  min_row,
+    max_row = max_row
+)
+
+categories = Reference(
+    sheet,
+    min_col = min_column,
+    max_col = min_column,
+    min_row =  min_row + 1, #para nao pegar o ano coloca min_row + 1 
+    max_row = max_row 
+)
+
+barchart.add_data(data, titles_from_data = True)
+barchart.set_categories(categories)
+
+# 4 - Criando o Gráfico
+sheet.add_chart(barchart, "B10")
+barchart.title = "Vendas por Fabricantes"
+barchart.style = 2
+
+# 5 - Salvando o Workbook
+wb.save("data/barchart.xlsx")
